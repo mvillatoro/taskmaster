5b02796a47d24f4f0af7d06033792b1574f7db2c
Mario <mvilla2208@gmail.com>
2024-09-26 12:25:22 -0600

    basic login

---
+++ src/app/login/login.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { createClient } from "../../../utils/supabase/components";
+
+export default function Login() {
+  const router = useRouter();
+  const supabase = createClient();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function logIn() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      console.error(error);
+    }
+    router.push("/");
+  }
+
+  async function signUp() {
+    const { error } = await supabase.auth.signUp({ email, password });
+    if (error) {
+      console.error(error);
+    }
+    router.push("/");
+  }
+
+  return (
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input
+        id="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="button" onClick={logIn}>
+        Log in
+      </button>
+      <button type="button" onClick={signUp}>
+        Sign up
+      </button>
+    </form>
+  );
+}
