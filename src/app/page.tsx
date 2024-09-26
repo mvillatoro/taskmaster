deb11147f1398722ff3cea149b2166569fa35c74
Mario <mvilla2208@gmail.com>
2024-09-25 23:32:35 -0600

    effing comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 
